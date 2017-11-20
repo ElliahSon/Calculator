92e9e0fc7b3144b4661c4715a2d493eaddddef43
ElliahSon <30949066+ElliahSon@users.noreply.github.com>
2017-11-20 11:21:35 -1000

    done

--- work.js
+++ work.js
@@ -1,4 +1,30 @@
 var output;
+var p = "For x your options are either: add, subtract, multiply, or divide with any 2 numbers for y and z.";
 function calculator (x, y ,z) {
-  return 
+  if (x == "add") {
+    output = y + z;
+
+  } else if (x == "subtract") {
+    output = y - z;
+
+  } else if (x == "multiply") {
+    output = y * z;
+
+  } else if (x == "divide")  {
+    output = y / z;
+
+  } else {
+    output = "Only input add, subtract, multiply, and divide with any number for y and z. Dummy."
+  }
 }
+calculator ("multipy", 24, 6);
+
+console.log(output);
+
+
+
+
+
+
+
+
